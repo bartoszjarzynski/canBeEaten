8aab1f32dc78a8831b8a1d9803e29224490267d4
Bartosz Jarzyński <bartjar978@student.polsl.pl>
2024-12-24 12:59:14 +0100

    Initial Commit

---
+++ CanBeEaten/CanBeEatenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanBeEatenApp.swift
+//  CanBeEaten
+//
+//  Created by Bartosz Jarzyński on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CanBeEatenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
